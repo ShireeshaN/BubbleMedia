557e1b59f18895ef3c96c99df7e153dd166c6d22
Sasikiran <sasikiransasi1234@gmail.com>
2024-06-12 10:53:17 +0530

    c

--- js/main.js
+++ js/main.js
@@ -99,10 +99,8 @@
             }
         }
     });
-    
-})(jQuery);
 
-// Contact form sending details to whatsapp
+    // Contact form sending details to whatsapp
 document.getElementById("contactForm").addEventListener("submit", function(event) {
     event.preventDefault(); // Prevent the form from submitting the traditional way
 
@@ -156,4 +154,39 @@ document.getElementById("contactForm").addEventListener("submit", function(event
         window.location.href = whatsappURL;
     }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var video = document.getElementById('myCanvas');
+
+    function tryPlayVideo() {
+        // Attempt to play the video
+        video.muted = true
+        var playPromise = video.play();
+
+        // Check if play promise is returned
+        if (playPromise !== undefined) {
+            playPromise.then(function() {
+                // Autoplay started!
+                video.muted = false; // Ensure the sound is on
+            }).catch(function(error) {
+                // Autoplay was prevented, possibly due to browser's autoplay policy
+                console.log('Autoplay was prevented: ', error);
+                video.muted = true; // Mute the video so it can play
+                video.play(); // Attempt to play again muted
+            });
+        }
+    }
+
+    video.addEventListener('canplaythrough', function() {
+        tryPlayVideo();
+    });
+
+    // Ensure the video is loaded
+    video.addEventListener('loadeddata', function() {
+        
+    });
+});
+
+    
+})(jQuery);
 
